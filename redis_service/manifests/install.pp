b333e5e52bee0b0ebc1db7dc9d6b57bb98155124
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2014-11-17 18:50:38 -0200

    using docker package in redis service

--- redis_service/manifests/install.pp
+++ redis_service/manifests/install.pp
@@ -1,13 +1,12 @@
 class redis_service::install (
-    $filer_docker,
     $lxc_docker_version = 'latest'
 ) inherits redis_service {
 
   include base
 
-  file { '/etc/profile.d/docker.sh' :
-    content => $docker_alias,
-    mode    => '0755'
+  class {'docker':
+    lxc_docker_version => $lxc_docker_version,
+    docker_bind_opts   => '-H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock',
   }
 
   file { '/etc/init/sentinel.conf':
@@ -18,34 +17,10 @@ class redis_service::install (
     mode    => '0644',
   }
 
-  if ( $lxc_docker_version == 'latest' ) {
-    $lxc_package_name = 'lxc-docker'
-    package { 'lxc-docker' :
-      ensure  => latest,
-      notify  => Service['docker']
-    }
-  } else {
-    $lxc_package_name = "lxc-docker-${lxc_docker_version}"
-    package { "lxc-docker-${lxc_docker_version}":
-      ensure => installed,
-      notify => Service['docker']
-    }
-  }
-
   package { 'redis-server':
     ensure  => latest,
     notify  => Service['sentinel'],
     require => File['/etc/init/sentinel.conf']
-  }
-
-  service { 'docker':
-    ensure     => running,
-    enable     => true,
-    hasrestart => true,
-    hasstatus  => true,
-    subscribe  => [File['/etc/init/docker.conf'], File['/etc/default/docker']],
-    provider   => 'upstart',
-    require    => [ Package[$lxc_package_name], File['/etc/init/docker.conf'], File['/etc/default/docker'] ]
   }
 
   service { 'sentinel':
@@ -60,23 +35,5 @@ class redis_service::install (
   service { 'redis-server':
     ensure  => stopped,
     require => Package['redis-server']
-  }
-
-  file { '/etc/init/docker.conf':
-    ensure  => present,
-    content => template('redis_service/init-docker.conf.erb'),
-    mode    => '0644',
-    owner   => root,
-    group   => root,
-    notify  => Service['docker']
-  }
-
-  file {'/etc/default/docker':
-    ensure  => file,
-    source  => 'puppet:///modules/redis_service/docker',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    notify  => Service['docker']
   }
 }
