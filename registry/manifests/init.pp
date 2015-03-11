bca76fd294924d4c63b720a1a9bd2e6ab7f1e74c
Wagner Souza <wagnersza@gmail.com>
2015-03-11 09:10:15 -0300

    registry: change vars default name

--- registry/manifests/init.pp
+++ registry/manifests/init.pp
@@ -5,22 +5,21 @@
 #
 # === Parameters
 #
-# [registry_ipbind_port] Registry ip X bind port
-# [registry_path] Registry local path
-# [registry_version] Registry package version
-# [registry_user]
-# [registry_group]
-# [registry_storage] storages: s3 glance swift glance-swift elliptics gcs local
-# [registry_venv_path] virtualenv to docker-registry installation
+# [ipbind_port] Registry ip X bind port
+# [path] Registry local path
+# [version] Registry package version
+# [user] Registre user name
+# [group] Registre group name
+# [storage] storages: s3 glance swift glance-swift elliptics gcs local
+# [venv_path] virtualenv to docker-registry installation
 
 class registry (
-  $registry_ipbind_port                = '0.0.0.0:8080',
-  $registry_path                       = '/var/lib/docker-registry',
-  $registry_version                    = latest,
-  $registry_user                       = 'registry',
-  $registry_group                      = 'registry',
-  $registry_storage                    = 'local',
-  $registry_venv_path                  = '/var/lib/venv',
+  $ipbind_port                         = '0.0.0.0:8080',
+  $version                             = latest,
+  $user                                = 'registry',
+  $group                               = 'registry',
+  $storage                             = 'local',
+  $venv_path                           = '/var/lib/venv',
   $gunicorn_max_requests               = 100,
   $gunicorn_workers                    = 3,
   $loglevel                            = 'info',
@@ -132,14 +131,14 @@ class registry (
     virtualenv => true,
   }
 
-  python::virtualenv { $registry_venv_path :
+  python::virtualenv { $venv_path :
     ensure       => present,
     version      => 'system',
     owner        => 'root',
     group        => 'root'
   }
 
-  file { "${registry_path}/config.yml":
+  file { "${path}/config.yml":
     ensure  => present,
     content => template('registry/config.yml.erb'),
     mode    => '0644',
@@ -156,12 +155,12 @@ class registry (
     notify  => Service['docker-registry'],
   }
 
-  if ( $registry_storage == local and mkdir_p($registry_path) ) {
-    file { $registry_path:
+  if ( $storage == local and mkdir_p($path) ) {
+    file { $path:
       ensure  => directory,
       mode    => '0755',
-      owner   => $registry_user,
-      group   => $registry_group,
+      owner   => $user,
+      group   => $group,
       notify  => Service['docker-registry']
     }
   }
